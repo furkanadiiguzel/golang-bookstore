03fa0797909b4d2614dfa3a1ffd47462aa059246
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-11-18 16:30:25 +0300

    controller finished

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -4,26 +4,64 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/furkanadiiguzel/golang-bookstore/pkg/models"
 	"github.com/furkanadiiguzel/golang-bookstore/pkg/utils"
+	"github.com/gorilla/mux"
 )
 
 var NewBook models.Book
 
 func GetBook(w http.ResponseWriter, r http.Request) {
-	books := models.GetBook()
-	res, _ := json.Marshal(books)
-	fmt.Println("Endpoint Hit: GetBook")
+	newBook := models.GetBook()
+	res, _ := json.Marshal(newBook)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
+
+func GetBookById(w http.ResponseWriter, r http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 
 }
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
-	utils.ParseBody(r, &NewBook)
+	newBook := &models.Book{}
+	utils.ParseBody(r, newBook)
 	createdBook := NewBook.CreateBooks()
 	res, _ := json.Marshal(createdBook)
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	bookDetails, db := models.GetBookById(ID)
+	if bookDetails.Name == "" {
+		fmt.Println("Book not found")
+	}
+	utils.ParseBody(r, &bookDetails)
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
 }
