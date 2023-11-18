632410d7d1753b0f5344089b4cd39a49dc77068c
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-11-18 16:13:09 +0300

    functions added

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -26,8 +26,19 @@ func (b *Book) CreateBooks() *Book {
 	return b
 }
 
-func GetBooks() []Book {
+func GetBook() []Book {
 	var Books []Book
 	db.Find(&Books)
 	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+	return book
 }
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -6,8 +6,8 @@ import (
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/books", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/books", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/books/{id}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
