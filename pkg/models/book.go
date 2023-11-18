54bc469f28c2f1c0295177a93cdc6f299755eb29
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-11-18 12:52:01 +0300

    models structured

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,21 @@
-package book
+package models
+
+import (
+	"github.com/furkanadiiguzel/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name   string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
