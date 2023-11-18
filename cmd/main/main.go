b1953df2062e08f8ff459cf951ef7101911536a8
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-11-18 12:57:12 +0300

    main added

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/furkanadiiguzel/golang-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
 func main() {
+	router := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(router)
+	log.Fatal(http.ListenAndServe(":8000", router))
 
 }
