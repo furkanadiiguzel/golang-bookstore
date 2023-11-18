74084047f530a94f4fa752d6f3e5c3f5058a4e0f
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-11-18 16:18:00 +0300

    controller function adding

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,29 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/furkanadiiguzel/golang-bookstore/pkg/models"
+	"github.com/furkanadiiguzel/golang-bookstore/pkg/utils"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r http.Request) {
+	books := models.GetBook()
+	res, _ := json.Marshal(books)
+	fmt.Println("Endpoint Hit: GetBook")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	utils.ParseBody(r, &NewBook)
+	createdBook := NewBook.CreateBooks()
+	res, _ := json.Marshal(createdBook)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
